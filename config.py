68e559e615a711c2e08a61bdd52d58f2ab691ee4
Dylan To <dylan.to@hotmail.com>
2023-03-16 15:45:30 +0800

    Format

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-load_dotenv(os.path.join(basedir, '.env'))
+load_dotenv(os.path.join(basedir, ".env"))
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'top_secret'
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "top_secret"

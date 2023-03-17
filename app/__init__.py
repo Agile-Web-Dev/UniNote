18305a03927c9a64199f751d041b38e8e0b132af
Jack Sun <jsun1590@gmail.com>
2023-03-17 21:20:15 +0800

    add socketio and sql alchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,19 @@
 from flask import Flask
+from flask_socketio import SocketIO
+from flask_sqlalchemy import SQLAlchemy
 
 from config import Config
+
+db = SQLAlchemy()
+socketio = SocketIO()
 
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
+
+    db.init_app(app)
+    socketio.init_app(app)
 
     from app.api.routes import bp as api_bp
 

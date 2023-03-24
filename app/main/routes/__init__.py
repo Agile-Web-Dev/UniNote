3916e71af993f38b50fed37fc26ce50882a8b0cb
Dylan To <dylan.to@hotmail.com>
2023-03-24 23:37:10 +0800

    isort format

--- app/main/routes/__init__.py
+++ app/main/routes/__init__.py
@@ -2,10 +2,4 @@ from flask import Blueprint
 
 bp = Blueprint("main", __name__)
 
-from app.main.routes import dashboard
-
-from app.main.routes import login
-
-from app.main.routes import chatroom
-
-from app.main.routes import forum
+from app.main.routes import chatroom, dashboard, forum, login

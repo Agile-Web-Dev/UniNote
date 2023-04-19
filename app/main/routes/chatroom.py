25b40df8998fb44a64b6822766e5b6f6b2fa1111
dct <dylan.to@hotmail.com>
2023-04-19 11:42:55 +0800

    Pass ci

--- app/main/routes/chatroom.py
+++ app/main/routes/chatroom.py
@@ -3,7 +3,7 @@ from flask_login import login_required
 
 from app import db
 from app.auth.utils import in_class
-from app.classes.routes.classes import get_class_users
+from app.classes.routes.classes import get_class_info
 
 from . import bp
 
@@ -13,7 +13,7 @@ from . import bp
 @in_class
 def chatroom(class_id):
     session["class_id"] = class_id
-    header_items = get_class_users(class_id)
+    header_items = get_class_info(class_id)
     return render_template(
         "chatroom.html",
         title="Chatroom",

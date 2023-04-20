10f23687858f65d25e5742c1a3d7f14fad53f348
Dylan To <dylan.to@hotmail.com>
2023-04-20 16:44:45 +0800

    Protect notes view

--- app/main/routes/notes.py
+++ app/main/routes/notes.py
@@ -1,9 +1,11 @@
 from flask import render_template
+from flask_login import login_required
 
 from . import bp
 
 
 @bp.route("/<class_id>/notes", methods=["GET"])
+@login_required
 def notes(class_id):
     return render_template(
         "notes.html",

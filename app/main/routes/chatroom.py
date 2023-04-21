82491f1e55e6ee7134e57f97ea26a170d1ce1972
dct <dylan.to@hotmail.com>
2023-04-21 13:48:54 +0800

    Probably add class links to template

--- app/main/routes/chatroom.py
+++ app/main/routes/chatroom.py
@@ -6,7 +6,7 @@ from app.auth.utils import in_class
 from app.classes.routes.classes import get_class_info
 from app.libs.filters import format_datetime
 from app.libs.processors import topbar
-from app.models import Message, load_user
+from app.models import Class, Message, load_user
 
 from . import bp
 
@@ -17,10 +17,27 @@ from . import bp
 def chatroom(class_id):
     session["class_id"] = class_id
     messages = Message.query.filter(Message.class_id == class_id)
+    class_links = [
+        {
+            "name": x,
+            "icon": "link",
+            "url": x,
+        }
+        for x in get_class_info()["links"]
+    ]
+
+    nav_items = [
+        {
+            "name": "Chat",
+            "icon": "bi-chat-left-dots-fill",
+            "url": f"/{class_id}/chatroom",
+        },
+        {"name": "Notes", "icon": "bi-pencil-square", "url": f"/{class_id}/notes"},
+    ] + class_links
 
     return render_template(
         "chatroom.html",
         title="Chatroom",
-        nav_items=["Chat", "Notes", "Labs", "Project", "Exam"],
+        nav_items=nav_items,
         messages=messages,
     )

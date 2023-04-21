f1d6cda3c3afbb98746b0b615a9e763e743d2f9a
Dylan To <dylan.to@hotmail.com>
2023-04-21 17:05:41 +0800

    Dynamic navbar links

--- app/main/routes/chatroom.py
+++ app/main/routes/chatroom.py
@@ -4,6 +4,7 @@ from flask_login import login_required
 from app import db
 from app.auth.utils import in_class
 from app.classes.routes.classes import get_class_info
+from app.classes.routes.links import get_class_links
 from app.libs.filters import format_datetime
 from app.libs.processors import topbar
 from app.models import Class, Message, load_user
@@ -17,14 +18,6 @@ from . import bp
 def chatroom(class_id):
     session["class_id"] = class_id
     messages = Message.query.filter(Message.class_id == class_id)
-    class_links = [
-        {
-            "name": x,
-            "icon": "link",
-            "url": x,
-        }
-        for x in get_class_info()["links"]
-    ]
 
     nav_items = [
         {
@@ -33,7 +26,7 @@ def chatroom(class_id):
             "url": f"/{class_id}/chatroom",
         },
         {"name": "Notes", "icon": "bi-pencil-square", "url": f"/{class_id}/notes"},
-    ] + class_links
+    ] + get_class_links(class_id)
 
     return render_template(
         "chatroom.html",
--- app/main/routes/notes.py
+++ app/main/routes/notes.py
@@ -1,14 +1,25 @@
 from flask import render_template
 from flask_login import login_required
 
+from app.classes.routes.links import get_class_links
+
 from . import bp
 
 
 @bp.route("/<class_id>/notes", methods=["GET"])
 @login_required
 def notes(class_id):
+    nav_items = [
+        {
+            "name": "Chat",
+            "icon": "bi-chat-left-dots-fill",
+            "url": f"/{class_id}/chatroom",
+        },
+        {"name": "Notes", "icon": "bi-pencil-square", "url": f"/{class_id}/notes"},
+    ] + get_class_links(class_id)
+
     return render_template(
         "notes.html",
         title="Note Forum",
-        nav_items=["Chat", "Notes", "Labs", "Project", "Exam"],
+        nav_items=nav_items,
     )

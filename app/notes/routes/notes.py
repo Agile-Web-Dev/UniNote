baa9137dd0cec2a17b14e5e3df46d0ad0f9d18cf
Jack Sun <jsun1590@gmail.com>
2023-04-24 19:25:08 +0800

    convention changes

--- app/notes/routes/notes.py
+++ app/notes/routes/notes.py
@@ -5,18 +5,18 @@ from . import bp
 
 
 @bp.route("/<class_id>", methods=["GET"])
-def getNotes(class_id):
+def get_notes(class_id):
     """get notes based on the classID"""
-    resArr = []
+    res_arr = []
     res = db.session.query(Note).filter(Note.class_id == class_id)
     for entry in res:
-        resArr.append(entry.serialize())
-    return resArr
+        res_arr.append(entry.serialize())
+    return res_arr
 
 
 # when user saves their notes
 @bp.route("/", methods=["POST"])
-def postNotes(createdBy, classId, title, content):
+def post_notes(createdBy, classId, title, content):
     """upload notes based on the classID into database"""
     note = Note(created_by=createdBy, class_id=classId, title=title, content=content)
     db.session.add(note)

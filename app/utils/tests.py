3953a618d993a5f4cbe83abb670c28aaa32d2382
Dylan To <dylan.to@hotmail.com>
2023-05-05 17:05:08 +0800

    Classes and links test

--- app/utils/tests.py
+++ app/utils/tests.py
@@ -1,9 +1,12 @@
-ignored_keys = ["note_id", "created_at", "updated_at"]
+ignored_keys = ["note_id", "created_at", "updated_at", "link_id"]
 
 
 def remove_keys(d, keys):
     r = dict(d)
 
     for key in keys:
-        del r[key]
+        try:
+            del r[key]
+        except Exception:
+            pass
     return r

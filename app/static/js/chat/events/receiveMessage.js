879188f0f1d3c5f77f30078ceba32a41aff32881
Jack Sun <jsun1590@gmail.com>
2023-05-21 14:50:58 +0800

    style chatroom

--- app/static/js/chat/events/receiveMessage.js
+++ app/static/js/chat/events/receiveMessage.js
@@ -1,11 +1,14 @@
 import { getAvatar } from "../../user.js";
 
 const messageHtml = `
-<div class="message d-flex gap-3 mb-3 d-none">
+<div class="message d-flex d-none gap-3">
   <div class="avatar-holder d-flex">
     <img class="avatar" preload src="/static/images/placeholder_avatar.png"/></div>
   <div class="d-flex flex-column sender">
-    <p class="name fw-bold mb-1"></p>
+    <p class="d-flex mb-1">
+      <span class="name fw-bold"></span>
+      <span></span>
+    </p>
     <div class="message-content"></div>
   </div>
 </div>`;
@@ -18,18 +21,14 @@ export const receiveMessage = async (message) => {
     lastAuthor = message.created_by;
     const messageElement = $(messageHtml);
     lastParentMessage = messageElement;
-    const avatar = messageElement
-      .children(".avatar-holder")
-      .children(".avatar");
+    const avatar = messageElement.find(".avatar");
     avatar.prop("src", await getAvatar(message.created_by));
     avatar.on("load", () => {
       messageElement.removeClass("d-none");
     });
     $(".sender").attr("id", "sender-" + message.created_by);
-    messageElement.children("div").children(".name").text(message.created_by);
-    const messageContainer = messageElement
-      .children("div")
-      .children(".message-content");
+    messageElement.find(".name").text(message.created_by);
+    const messageContainer = messageElement.find(".message-content");
     $(`<p class="message-item mb-0" id="message-${message.message_id}"></p>`)
       .text(message.content)
       .appendTo(messageContainer);
@@ -37,9 +36,7 @@ export const receiveMessage = async (message) => {
     messageElement.appendTo("#chat-scroll-window");
   } else {
     $(".sender").attr("id", "sender-" + message.created_by);
-    const messageContainer = lastParentMessage
-      .children("div")
-      .children(".message-content");
+    const messageContainer = lastParentMessage.find(".message-content");
     $("<b></b>").appendTo(messageContainer);
     $(`<p class="message-item mb-0" id="message-${message.message_id}"></p>`)
       .text(message.content)

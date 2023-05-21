8b6599cbe2540999181fcf217f39c480fdb3c419
Jack Sun <jsun1590@gmail.com>
2023-05-21 16:47:15 +0800

    timestamp

--- app/static/js/chat/events/receiveMessage.js
+++ app/static/js/chat/events/receiveMessage.js
@@ -1,5 +1,16 @@
 import { getAvatar } from "../../user.js";
 
+const units = {
+  year: 24 * 60 * 60 * 1000 * 365,
+  month: (24 * 60 * 60 * 1000 * 365) / 12,
+  day: 24 * 60 * 60 * 1000,
+  hour: 60 * 60 * 1000,
+  minute: 60 * 1000,
+  second: 1000,
+};
+
+const rtf = new Intl.RelativeTimeFormat("en", { numeric: "auto" });
+
 const messageHtml = `
 <div class="message d-flex d-none gap-3">
   <div class="avatar-holder d-flex">
@@ -7,11 +18,36 @@ const messageHtml = `
   <div class="d-flex flex-column sender">
     <p class="d-flex mb-1">
       <span class="name fw-bold"></span>
-      <span></span>
+      <span class="bot"></span>
+      <span class="timestamp"></span>
     </p>
     <div class="message-content"></div>
   </div>
 </div>`;
+
+const formatTimestamp = (timestamp) => {
+  const datetime = new Date(timestamp);
+  const now = new Date();
+
+  const timeDiff = datetime-now;
+
+  let formattedDatetime = "";
+  if (timeDiff < 24 * 60 * 60 * 1000) {
+    formattedDatetime =
+      "— Today at " +
+      datetime.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+  } else if (timeDiff < 2 * 24 * 60 * 60 * 1000) {
+    formattedDatetime =
+      "— Yesterday at " +
+      datetime.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+  } else {
+    formattedDatetime =
+      datetime.toLocaleDateString() +
+      " at " +
+      datetime.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+  }
+  return formattedDatetime;
+};
 
 let lastParentMessage;
 let lastAuthor = "";
@@ -27,7 +63,19 @@ export const receiveMessage = async (message) => {
       messageElement.removeClass("d-none");
     });
     $(".sender").attr("id", "sender-" + message.created_by);
+
     messageElement.find(".name").text(message.created_by);
+
+    if (message.is_bot) {
+      const bot = messageElement.find(".bot");
+      bot.text(" (verified)");
+      bot.css("color", "#dead73");
+    }
+
+    messageElement
+      .find(".timestamp")
+      .text(` ${formatTimestamp(message.created_at)}`);
+
     const messageContainer = messageElement.find(".message-content");
     $(`<p class="message-item mb-0" id="message-${message.message_id}"></p>`)
       .text(message.content)
